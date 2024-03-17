6e94f05429adc7c0d0d4e4c7c506d218674639a6
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2024-03-17 01:01:37 -0500

    Troubleshooting continues...

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
 
+import drivetrain
+
 # Create your objects here
 
 # Initialize the EV3 Brick.
 ev3 = EV3Brick()
 
 # Initialize a motor at port B.
-# test_motor = Motor(Port.B)
+test_motor = Motor(Port.S1)
 
 # Write your program here
 
@@ -18,7 +20,7 @@ ev3 = EV3Brick()
 # ev3.speaker.beep()
 
 # Run the motor up to 500 degrees per second. To a target angle of 90 degrees.
-# test_motor.run_target(500, 90)
+test_motor.run_target(500, 90)
 
 # Play another beep sound.
 
@@ -26,3 +28,6 @@ print('Hello, world!')
 
 ev3.speaker.beep(frequency=1000, duration=500)
 
+if __name__ == '__main__':
+    print("Started.")
+    drivetrain.test_func()
