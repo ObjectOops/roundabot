cd6d760a7f522f6682dcc8e07c62098417fb85c8
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2024-03-23 14:50:47 -0500

    Fix manual.

--- extra/manual.py
+++ extra/manual.py
@@ -1,40 +1,42 @@
-import termios
-import select
-import sys
+#!/usr/bin/env pybricks-micropython
+
+# import termios
+# import select
+# import sys
 import os
 
 from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port, Direction
 from pybricks.robotics import DriveBase
-from pybricks.parameters import Port
 from pybricks.tools import wait
 
-original_state = None
+# original_state = None
 
-def stdin_immediate():
-    global original_state
-    if original_state is None:
-        original_state = termios.tcgetattr(sys.stdin)
-    new_state = termios.tcgetattr(sys.stdin)
-    new_state[3] &= ~termios.ICANON # No buffering.
-    new_state[3] &= ~termios.ECHO # Suppress echoing.
-    new_state[6][termios.VMIN] = 1
-    new_state[6][termios.VTIME] = 0
-    termios.tcsetattr(sys.stdin, termios.TCSANOW, new_state)
+# def stdin_immediate():
+#     global original_state
+#     if original_state is None:
+#         original_state = termios.tcgetattr(sys.stdin)
+#     new_state = termios.tcgetattr(sys.stdin)
+#     new_state[3] &= ~termios.ICANON # No buffering.
+#     new_state[3] &= ~termios.ECHO # Suppress echoing.
+#     new_state[6][termios.VMIN] = 1
+#     new_state[6][termios.VTIME] = 0
+#     termios.tcsetattr(sys.stdin.fileno(), termios.TCSANOW, new_state)
 
-def stdin_reset():
-    global original_state
-    if original_state is None:
-        print("Failed to reset terminal state.")
-        return
-    termios.tcsetattr(sys.stdin, termios.TCSANOW, original_state)
-    print("Reset terminal state.")
+# def stdin_reset():
+#     global original_state
+#     if original_state is None:
+#         print("Failed to reset terminal state.")
+#         return
+#     termios.tcsetattr(sys.stdin, termios.TCSANOW, original_state)
+#     print("Reset terminal state.")
 
-def getch() -> str:
-    return str(os.read(sys.stdin.fileno(), 1), encoding="utf8")
+# def getch() -> str:
+#     return str(os.read(sys.stdin.fileno(), 1), encoding="utf8")
 
-def kbhit() -> bool:
-    is_set, _, _ = select.select((sys.stdin), (), (), 0)
-    return sys.stdin in is_set
+# def kbhit() -> bool:
+#     is_set, _, _ = select.select((sys.stdin), (), (), 0)
+#     return sys.stdin in is_set
 
 def rgb2hsv(r: float, g: float, b: float) -> tuple[float, float, float]:
     v = ma = max(r, g, b)
@@ -53,45 +55,53 @@ def rgb2hsv(r: float, g: float, b: float) -> tuple[float, float, float]:
     return (h, s, v)
 
 if __name__ == "__main__":
-    stdin_immediate()
+    # stdin_immediate()
 
-    left_motor = Motor(Port.D)
-    right_motor = Motor(Port.A)
-    steering_motor = Motor(Port.B)
+    left_motor = Motor(Port.D, positive_direction=Direction.COUNTERCLOCKWISE)
+    right_motor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
+    steering_motor = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
     
-    drive_base = DriveBase(left_motor, right_motor, 50, 10)
+    drive_base = DriveBase(left_motor, right_motor, 43, 170)
 
-    color_sensor = ColorSensor(Port.S1)
+    color_sensor = ColorSensor(Port.S4)
 
     driving = False
-    speed = 5
-    turn = 5
+    speed = 100
+    turn = 25
+    speed_map = {
+        'w' : (speed, 0), 
+        'a' : (0, -turn), 
+        's' : (-speed, 0), 
+        'd' : (0, turn)
+    }
     while True:
-        c = getch() if kbhit() else None
-        match c:
-            case 'w', 'a', 's', 'd':
-                if driving:
-                    drive_base.stop()
-                    driving = False
-                else:
-                    driving = True
-            case 'w', 's':
-                steering_motor.track_target(0)
-                wait(250)
-            case 'a', 'd':
-                steering_motor.track_target(90)
-                wait(250)
-            case 'w':
-                drive_base.drive(speed, 0)
-            case 'a':
-                drive_base.drive(0, -turn)
-            case 's':
-                drive_base.drive(-speed, 0)
-            case 'd':
-                drive_base.drive(0, turn)
-            case 'i':
-                print(f"Position: {drive_base.position()}\nAngle: {drive_base.angle()}\nColor Sensor: {(color_sensor.color(), color_sensor.ambient(), color_sensor.reflection(), color_sensor.rgb(), rgb2hsv(color_sensor.rgb()))}")
-            case 'q':
-                break
+        c = input()
+        if c in "wasd" and len(c) != 0:
+            if driving:
+                drive_base.stop()
+                driving = False
+            else:
+                driving = True
+                if c in "ws":
+                    steering_motor.track_target(0)
+                    wait(250)
+                if c == 'a':
+                    steering_motor.track_target(-90)
+                    wait(250)
+                elif c == 'd':
+                    steering_motor.track_target(90)
+                    wait(250)
+                print("Key:", c)
+                speed, turn = speed_map[c]
+                drive_base.drive(speed, turn)
+        elif c == 'i':
+            r, g, b = color_sensor.rgb()
+            print("Position:", drive_base.distance(), "\nAngle:", drive_base.angle(), "\nColor Sensor:", (color_sensor.color(), color_sensor.ambient(), color_sensor.reflection(), color_sensor.rgb(), rgb2hsv(r, g, b)))
+        elif c == 'q':
+            drive_base.stop()
+            wait(250)
+            steering_motor.track_target(0)
+            wait(250)
+            break
 
-    stdin_reset()
+    # stdin_reset()
